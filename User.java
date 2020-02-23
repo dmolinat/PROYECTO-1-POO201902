8336fdaf7ca4553f4d0b39e0c7c3072fe4c96387
dmolinat <55594409+dmolinat@users.noreply.github.com>
2020-02-23 09:15:32 -0500

    Revert "Interfaz Usuario"
    This reverts commit fb8cc3193cf4f2bd6acd87bea4e24e3a6563329c.

--- User.java
+++
@@ -1,9 +0,0 @@
-package gestorAplicacion.users;
-import java.util.LinkedList;
-public interface User {
-	
-	public String descripTeatro();
-	public LinkedList<Funcion> verF();
-	public boolean crearUser();
-	
-}
