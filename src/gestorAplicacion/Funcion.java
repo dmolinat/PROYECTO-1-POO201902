d892f71b64d7ab32ece45c50950dc275c94aaa80
wgarciab <55595765+wgarciab@users.noreply.github.com>
2020-03-02 16:25:41 -0500

    Se añadieron dos nuevos metodos, se definió metodo getAsientos(), se cambio Date por Calendar
    Metodos añadidos: buscarAsiento(), verAsientosDisponibles()

--- src/gestorAplicacion/Funcion.java
+++ src/gestorAplicacion/Funcion.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class Funcion {
     Pelicula pelicula;
     Sala sala;
-    Date fecha;
+    Calendar fecha;
     ArrayList<Asiento> asientos;
     public String verPelicula(){
         return pelicula.getNombre();
@@ -15,11 +15,40 @@ public class Funcion {
     public int getSala(){
         return sala.getID();
     }
-    public Date getfecha(){
+    public Calendar getfecha(){
         return fecha;
     }
-    public void getAsientos(){
-       // falta definirla
+    public ArrayList<Asiento> getAsientos(){
+       return asientos;
         
+    }
+    
+    public Asiento buscarAsiento(String codigo) {
+		Iterator<Asiento> iterador = asientos.iterator();
+		while(iterador.hasNext()) {
+			Asiento asiento = (Asiento) iterador.next();
+			if (asiento.codigo.equals(codigo)) {
+				return asiento;
+			}
+		}
+		return null;
+	}
+    
+    public boolean verAsientosDisponibles() {
+    	int contador = 0;
+    	Iterator<Asiento> iterador = asientos.iterator();
+		while(iterador.hasNext()) {
+			Asiento asiento = (Asiento) iterador.next();
+			if (asiento.disponible == true) {
+				System.out.println(asiento.codigo);
+				contador++;
+			}
+		}
+		if (contador > 0) {
+			return true; //hay asientos disponibles
+		}
+		else {
+			return false; //no hay asientos disponibles
+		}
     }
 }
