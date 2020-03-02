5fa53ef397a64649fb2bf5cc4f49930ae0535f2d
dmolinat <55594409+dmolinat@users.noreply.github.com>
2020-03-02 14:47:06 -0500

    Add files via upload

--- Start.java
+++ Start.java
@@ -1,41 +1,52 @@
 package uiMain;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import uiMain.menuconsola.OpcionDeMenu;
+import uiMain.menuconsola.Login;
+import uiMain.menuconsola.newUser;
 import gestorAplicacion.users.Admin_System;
 import gestorAplicacion.users.Cliente;
 import gestorAplicacion.users.User_Invi;
 import gestorAplicacion.users.User_R;
-import java.util.Scanner;
+import uiMain.menuconsola.InInv;
+import uiMain.menuconsola.MenuDeConsola;
+
 import gestorAplicacion.DatosTeatro;
 
 public class Start {
+	
 	public static void main(String args[]) {
+		Start.On();
+		Start.Off();
+	}
+	
+	public static void On() {
+		
+		(MenuDeConsola.OP1).add(new Login());(MenuDeConsola.OP1).add(new InInv());(MenuDeConsola.OP1).add(new newUser());
 		Scanner in = new Scanner(System.in);
 		int op=-1;
-		String user;
-		String pass;
-		
 		System.out.println("Bienvenido a la APP de Teatros SA");
 		do {
-			System.out.println("Presione 0: Para iniciar sesi√≥n.");
-			System.out.println("Presiona 1: Para entrar como invitado.");
-			System.out.println("Presiona 2: Para registrarse.");
+			for(int i=0;i<(MenuDeConsola.OP1).size();i++) {
+				System.out.println("Presiona "+i+": para "+((MenuDeConsola.OP1).get(i)).mostrar());
+			}
+			System.out.println("Presiona 3: para Salir");
 			op=in.nextInt();
-			if(op!=0 && op!=1 && op!=2) {
-				System.out.println("ERROR: Esta opci√≥n no est√° disponible");
+			
+			if(op!=3) {
+				((MenuDeConsola.OP1).get(op)).ejecutar();
 			}
 			
-		}while(op!=0 && op!=1 && op!=2);
+			if(op!=0 && op!=1 && op!=2 && op!=3) {
+				System.out.println("ERROR: Esta opciÛn no est· disponible");
+			}
+		}while(op!=3);
 		
-		if(op==0) {
-			System.out.print("User: "); user=in.next();
-			System.out.print("Pass: "); pass=in.next();
-			//Codigo para reconocer si es Cliente o Admin
-		}else if(op==1) {
-			(DatosTeatro.Ui).add(new User_Invi());
-		}else{
-			(DatosTeatro.U).add((new Cliente()).crearUser());
-			//No importa quien llame la funci√≥n porque
-			//al ejecutarse el m√©todo crearUser, va a devolver un nuevo objeto.
-			//new Cliente() ac√° se usa como una instancia auxiliar
-		}
+	}
+	
+	public static void Off() {
+		System.out.println("!!AVISO: Programa Acabado°°");
 	}
 }
