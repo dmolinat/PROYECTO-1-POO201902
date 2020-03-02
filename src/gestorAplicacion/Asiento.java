03119c0ec84325aa0657154009404fde68d27a24
wgarciab <55595765+wgarciab@users.noreply.github.com>
2020-03-02 16:20:52 -0500

    Creada Clase Asiento Version 1

---
+++ src/gestorAplicacion/Asiento.java
@@ -0,0 +1,41 @@
+package gestorAplicacion;
+
+public class Asiento {
+	
+	String codigo;
+	int precio;
+	boolean disponible = true;
+	
+	Asiento (String codigo, int precio, boolean disponible){
+		this.codigo = codigo;
+		this.precio = precio;
+		this.disponible = disponible;
+	}
+
+	//setter y getters
+	
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+	
+}
