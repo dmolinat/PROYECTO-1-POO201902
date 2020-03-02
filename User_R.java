daa4e117d4a11b36bbf4f0ed5c6c9c63566dc4d7
dmolinat <55594409+dmolinat@users.noreply.github.com>
2020-03-02 14:49:43 -0500

    Add files via upload

--- User_R.java
+++ User_R.java
@@ -2,6 +2,7 @@ package gestorAplicacion.users;
 import java.util.ArrayList;
 import java.util.Scanner;
 import gestorAplicacion.Funcion;
+import gestorAplicacion.DatosTeatro;
 
 public abstract class User_R implements User{
 	private String nombre;
@@ -36,38 +37,24 @@ public abstract class User_R implements User{
 	}
 	
 	//Crear USUARIO:
-	public User_R crearUser() {
+	public User_R crearUser(int Op) {
 		Scanner in = new Scanner(System.in);
-		int op;
-		do {
-			System.out.println("Ingrese la opción según el tipo de cuenta que desee ser: ");
-			System.out.println("1. Crear cuenta como administrador");
-			System.out.println("2. Crear cuenta común");
-			op=in.nextInt();
-			if(op!=1 && op!=2) {
-				System.out.println("¡¡ERROR: LA OPCIÓN NO ESTÁ DISPONIBLE!!");
-			}
-			
-		}while(op!=1 && op!=2);
-		
-		//CREAR USUARIO DE TIPO ADMIN_SYSTEM
-		if(op==1) {
-			ArrayList<Object> D = new ArrayList();
-			String aux;Long aux1; Integer aux2;
+		ArrayList<Object> D = new ArrayList();
+		String aux;Long aux1; Integer aux2;
 			//Ingresar el nombre
-			do {
+		do {
 				System.out.print("Ingrese el nombre: "); aux=in.next();
 				if(aux.equals("")||aux.contains(" ")) {
 					System.out.println("===========================");
-					System.out.println("!!ERROR: Nombre invalido¡¡");
+					System.out.println("!!ERROR: Nombre invalido��");
 					System.out.println("===========================");
 					aux=null;
-				}else if(aux.contains("0")||aux.contains("1")||aux.contains("2")||
+		}else if(aux.contains("0")||aux.contains("1")||aux.contains("2")||
 						aux.contains("3")||aux.contains("4")||aux.contains("5")||
 						aux.contains("6")||aux.contains("7")||aux.contains("8")||
 						aux.contains("9")) {
 					System.out.println("=========================================================");
-					System.out.println("!!ERROR: Los nombres y apellidos no deben llevar numero¡¡");
+					System.out.println("!!ERROR: Los nombres y apellidos no deben llevar numero��");
 					System.out.println("=========================================================");
 					aux=null;
 				}	
@@ -81,7 +68,7 @@ public abstract class User_R implements User{
 				
 				if(aux.equals("")||aux.contains(" ")) {
 					System.out.println("============================");
-					System.out.println("!!ERROR: Apellido invalido¡¡");
+					System.out.println("!!ERROR: Apellido invalido��");
 					System.out.println("============================");
 					aux=null;
 				}else if(aux.contains("0")||aux.contains("1")||aux.contains("2")||
@@ -89,7 +76,7 @@ public abstract class User_R implements User{
 						aux.contains("6")||aux.contains("7")||aux.contains("8")||
 						aux.contains("9")) {
 					System.out.println("=========================================================");
-					System.out.println("!!ERROR: Los nombres y apellidos no deben llevar numeros¡¡");
+					System.out.println("!!ERROR: Los nombres y apellidos no deben llevar numeros��");
 					System.out.println("=========================================================");
 					aux=null;
 				}	
@@ -100,12 +87,12 @@ public abstract class User_R implements User{
 			aux=null;
 			do {
 				System.out.print("Ingrese un usuario: ");aux=in.next();
-				if(aux.equals("")||aux.contains(" ")){
-					System.out.println("!!ERROR: Login invalido¡¡");
+				if(aux.equals("")||aux.contains(" ")||existId(aux)){
+					System.out.println("!!ERROR: Login invalido��");
 					aux=null;
 				}
 				if(aux.length()<5) {
-					System.out.println("¡¡ERROR: No supera el número de caracteres¡¡");
+					System.out.println("��ERROR: No supera el n�mero de caracteres��");
 					aux=null;
 				}
 			}while(aux==null);
@@ -114,13 +101,13 @@ public abstract class User_R implements User{
 			//Ingrese Pass
 			aux=null;
 			do {
-				System.out.print("Ingrese una contraseña: ");aux=in.next();
+				System.out.print("Ingrese una contrase�a: ");aux=in.next();
 				if(aux.equals("")||aux.contains(" ")){
-					System.out.println("!!ERROR: Login invalido¡¡");
+					System.out.println("!!ERROR: Login invalido��");
 					aux=null;
 				}
 				if(aux.length()<6) {
-					System.out.println("¡¡ERROR: No supera el número de caracteres¡¡");
+					System.out.println("��ERROR: No supera el n�mero de caracteres��");
 					aux=null;
 				}
 				
@@ -129,10 +116,11 @@ public abstract class User_R implements User{
 			
 			//Ingresar Documento:
 			do {
+				System.out.print("Ingrese su Documento: ");
 				try {
-					System.out.print("Ingrese su Documento: ");aux1=in.nextLong();
-				}catch(Exception e) { //Buscar el tipo de error específico
-					System.out.println("!!ERROR: Solo se pueden ingresar numeros¡¡");
+					aux1=in.nextLong();
+				}catch(Exception e) { //Buscar el tipo de error espec�fico
+					System.out.println("!!ERROR: Solo se pueden ingresar numeros�� "+e);
 					aux1=null;
 				}
 			}while(aux1==null);
@@ -145,17 +133,17 @@ public abstract class User_R implements User{
 					System.out.print("Ingrese su Edad: ");aux2=in.nextInt();
 					
 					if(aux2<18) {
-						System.out.println("!!ERROR: No eres mayor de edad¡¡");
+						System.out.println("!!ERROR: No eres mayor de edad��");
 						aux2=null;
 					}
 					
 					if(aux2>199) {
-						System.out.println("!!ERROR: Edad Invalida¡¡");
+						System.out.println("!!ERROR: Edad Invalida��");
 						aux2=null;
 					}
 					
-				}catch(Exception e) { //Buscar el tipo de error específico
-					System.out.println("!!ERROR: Solo se pueden ingresar numeros¡¡");
+				}catch(Exception e) { //Buscar el tipo de error espec�fico
+					System.out.println("!!ERROR: Solo se pueden ingresar numeros��");
 					aux2=null;
 				}
 			}while(aux2==null);
@@ -166,136 +154,39 @@ public abstract class User_R implements User{
 			do {
 				System.out.print("Ingrese su sexo: ");aux=in.next();
 				if((!aux.equals("M")&&!aux.equals("F"))||aux.equals("")||aux.contains(" ")) {
-					System.out.println("!!ERROR: Sexo Invalido¡¡");
+					System.out.println("!!ERROR: Sexo Invalido��");
 					aux=null;
 				}
 			}while(aux==null);
 			D.add(aux);
 			
 			//Crear el Admin_System:
-			return new Admin_System((String) D.get(0),(String)D.get(1),
-					(String)D.get(2),(String)D.get(3),(Long)D.get(4),
-					(Integer)D.get(5),(String)D.get(6));
-			
+			if(Op==1) {
+				return new Admin_System((String) D.get(0),(String)D.get(1),
+						(String)D.get(2),(String)D.get(3),(Long)D.get(4),
+						(Integer)D.get(5),(String)D.get(6));
+
+			}else {
+				return new Cliente((String) D.get(0),(String)D.get(1),
+						(String)D.get(2),(String)D.get(3),(Long)D.get(4),
+						(Integer)D.get(5));
+			}
+						
 			
 		//USUARIO CLIENTE	
-		}else {
-			ArrayList<Object> D = new ArrayList();
-			String aux;Long aux1; Integer aux2;
 			
-			//Ingresar el nombre
-			do {
-				System.out.print("Ingrese el nombre: "); aux=in.next();
-				if(aux.equals("")||aux.contains(" ")) {
-					System.out.println("===========================");
-					System.out.println("!!ERROR: Nombre invalido¡¡");
-					System.out.println("===========================");
-					aux=null;
-				}else if(aux.contains("0")||aux.contains("1")||aux.contains("2")||
-						aux.contains("3")||aux.contains("4")||aux.contains("5")||
-						aux.contains("6")||aux.contains("7")||aux.contains("8")||
-						aux.contains("9")) {
-					System.out.println("=========================================================");
-					System.out.println("!!ERROR: Los nombres y apellidos no deben llevar numero¡¡");
-					System.out.println("=========================================================");
-					aux=null;
-				}	
-			}while(aux==null);
-			D.add(aux);
-			
-			//Ingresar el apellido
-			aux=null;
-			do {
-				System.out.print("Ingrese el apellido: ");aux=in.next();
-				
-				if(aux.equals("")||aux.contains(" ")) {
-					System.out.println("============================");
-					System.out.println("!!ERROR: Apellido invalido¡¡");
-					System.out.println("============================");
-					aux=null;
-				}else if(aux.contains("0")||aux.contains("1")||aux.contains("2")||
-						aux.contains("3")||aux.contains("4")||aux.contains("5")||
-						aux.contains("6")||aux.contains("7")||aux.contains("8")||
-						aux.contains("9")) {
-					System.out.println("=========================================================");
-					System.out.println("!!ERROR: Los nombres y apellidos no deben llevar numeros¡¡");
-					System.out.println("=========================================================");
-					aux=null;
-				}	
-			}while(aux==null);
-			D.add(aux);
-			
-			//Ingrese Login
-			aux=null;
-			do {
-				System.out.print("Ingrese un usuario: ");aux=in.next();
-				if(aux.equals("")||aux.contains(" ")){
-					System.out.println("!!ERROR: Login invalido¡¡");
-					aux=null;
-				}
-				if(aux.length()<5) {
-					System.out.println("¡¡ERROR: No supera el número de caracteres¡¡");
-					aux=null;
-				}
-			}while(aux==null);
-			D.add(aux);
-			
-			//Ingrese Pass
-			aux=null;
-			do {
-				System.out.print("Ingrese una contraseña: ");aux=in.next();
-				if(aux.equals("")||aux.contains(" ")){
-					System.out.println("!!ERROR: Login invalido¡¡");
-					aux=null;
-				}
-				if(aux.length()<6) {
-					System.out.println("¡¡ERROR: No supera el número de caracteres¡¡");
-					aux=null;
-				}
-				
-			}while(aux==null);
-			D.add(aux);
-			
-			//Ingresar Documento:
-			do {
-				try {
-					System.out.print("Ingrese su Documento: ");aux1=in.nextLong();
-				}catch(Exception e) { //Buscar el tipo de error específico
-					System.out.println("!!ERROR: Solo se pueden ingresar numeros¡¡");
-					aux1=null;
-				}
-			}while(aux1==null);
-			D.add(aux1);
-			
-			//Ingresar edad:
-			do {
-				try {
-					System.out.print("Ingrese su Edad: ");aux2=in.nextInt();
-					
-					if(aux2<18) {
-						System.out.println("!!ERROR: No eres mayor de edad¡¡");
-						aux2=null;
-					}
-					
-					if(aux2>199) {
-						System.out.println("!!ERROR: Edad Invalida¡¡");
-						aux2=null;
-					}
-				}catch(Exception e) { //Buscar el tipo de error específico
-					System.out.println("!!ERROR: Solo se pueden ingresar numeros¡¡");
-					aux2=null;
-				}
-			}while(aux2==null);
-			D.add(aux2);
-			
-			return new Cliente((String) D.get(0),(String)D.get(1),
-					(String)D.get(2),(String)D.get(3),(Long)D.get(4),
-					(Integer)D.get(5));
 		}
 		
-	}
 	
-	//Método para comprobar que los caracteres no contegan números y solamente espacios vacios
+	//M�todo para comprobar que el usuario ya existe.
+	private boolean existId(String aux) {
+		for(int i=0; i < (DatosTeatro.U).size();i++) {
+			if((((DatosTeatro.U).get(i)).getLogin()).equals(aux)) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
 	
 	//Get y Set
@@ -314,14 +205,14 @@ public abstract class User_R implements User{
 	}
 	
 	public String getPass() {
-		return this.nombre;
+		return this.pass;
 	}
 	public void setPass(String pass) {
 		this.pass=pass;
 	}
 	
 	public String getLogin() {
-		return this.nombre;
+		return this.login;
 	}
 	public void setLogin(String login) {
 		this.login=login;
