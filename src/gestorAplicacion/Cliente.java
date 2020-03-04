78457eaf5f711f86ae9b29afa76e45439a77e371
KeithKelsy <55594418+KeithKelsy@users.noreply.github.com>
2020-03-03 20:32:57 -0500

    Update Cliente.java

--- src/gestorAplicacion/Cliente.java
+++ src/gestorAplicacion/Cliente.java
@@ -1,23 +1,52 @@
 package gestorAplicacion.users;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
+import gestorAplicacion.DatosTeatro;
 import gestorAplicacion.Funcion;
+import uiMain.menuconsola.MenuDeConsola;
+import uiMain.menuconsola.OpcionDeMenu;
 
 public class Cliente extends User_R {
-	Cliente(String nombre, String apellido, String login, String pass, Long document,Integer edad){
-		this.nombre=nombre;
-		this.apellido=apellido;
-		this.login=login;
-		this.pass=pass;
-		this.document=document;
-		this.edad=edad;	
+	
+	//Añadir todas las opciones de menu que existan, pero el Cliente solo podrá
+	//acceder a las que estén con available=true
+	
+	private MenuDeConsola MenCli = new MenuDeConsola();
+	
+	public Cliente() {	
 	}
-
+	
+	public Cliente(String nombre,String apellido,String login,String pass, String document, Integer edad) {
+		super(nombre,apellido,login,pass,document,edad);
+	}
 	public String descripTeatro() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public void Opci() {
+		Scanner in = new Scanner(System.in);
+		int op=0;
+		do {
+			MenuDeConsola.lanzarMenu(((Admin_System)DatosTeatro.userLive).getMenAdmin().getOp());
+			System.out.println("Presiona "+ ((Admin_System)DatosTeatro.userLive).getMenAdmin().getOp().size() + ": Para salir");
+			op=in.nextInt();
+			
+			if(op!=((Admin_System)DatosTeatro.userLive).getMenAdmin().getOp().size()) {
+				((Admin_System)DatosTeatro.userLive).getMenAdmin().getOp().get(op).ejecutar();
+			}
+		}while(op!=((Admin_System)DatosTeatro.userLive).getMenAdmin().getOp().size());	
+	}
+	public 
+	
+	
+	
+	public MenuDeConsola getMenCli(){
+		return this.MenCli;
+	}
+	
 
 	public ArrayList<Funcion> verF() {
 		// TODO Auto-generated method stub
