8d89d72ce4c0ba4638d5df670bb1a4abf7a7d62b
wgarciab <55595765+wgarciab@users.noreply.github.com>
2020-03-04 05:38:58 -0500

    Update Reembolso.java
    Correccion de errores

--- src/uiMain/menuconsola/Reembolso.java
+++ src/uiMain/menuconsola/Reembolso.java
@@ -2,22 +2,26 @@ package uiMain.menuconsola;
 
 import java.util.Scanner;
 
+import gestorAplicacion.DatosTeatro;
 import gestorAplicacion.transaccion.Pago;
 import gestorAplicacion.transaccion.Tiquete;
 import gestorAplicacion.users.Cliente;
-import uiMain.OpcionDeMenu;
+import uiMain.menuconsola.OpcionDeMenu;
+import gestorAplicacion.transaccion.Tarjeta_des;
 
 public class Reembolso extends OpcionDeMenu {
 
-	public void mostrar() {
+	public String mostrar() {
+		return "Solicitar reembolso y cancelar asistencia: ";
 	}
 	
 	public void ejecutar() {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		Cliente usuario; //esta variable hay que apuntarla al cliente que esta loggeado
+		Cliente usuario = (Cliente) DatosTeatro.userLive; //esta variable hay que apuntarla al cliente que esta loggeado
 		
+		if(!usuario.getTiquetes().isEmpty()) {
 		System.out.println("Lista de sus tiquetes: ");
 		usuario.mostrarTiquetes();
 		System.out.println("Digite el numero de la lista, correspondiente al tiquete al cual desea solicitar un reembolso y cancelar su asistencia");
@@ -29,14 +33,23 @@ public class Reembolso extends OpcionDeMenu {
 		
 		Tarjeta_des tarjeta = usuario.getAfiliacion();
 		
-		boolean operacion = pago.reembolso(tarjeta);
-		if (operacion == true) {
-			System.out.println("Reembolso exitoso!");
-			System.out.println("Asistencia cancelada correctamente");
+		if(pago.isPagado()) {
+			boolean operacion = pago.reembolso(tarjeta);
+			if (operacion == true) {
+				System.out.println("Reembolso exitoso!");
+				System.out.println("Asistencia cancelada correctamente");
+			}
+			else {
+				System.out.println("Lo sentimos, faltan 24 horas o menos para el evento. No aplicas para reembolso");
+				System.out.println("Asistencia cancelada correctamente");
+			}
+		}else {
+			System.out.println("No se ha efectuado el pago del tiquete primero");
 		}
-		else {
-			System.out.println("Lo sentimos, faltan 24 horas o menos para el evento. No aplicas para reembolso");
-			System.out.println("Asistencia cancelada correctamente");
+		
+		
+		}else {
+			System.out.println("No posee tiquetes disponibles a pagar");
 		}
 	}
 }
