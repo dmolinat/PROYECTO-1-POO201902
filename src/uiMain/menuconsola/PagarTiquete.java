700341cd572cbfe961103655bbf8c570a614532c
dmolinat <55594409+dmolinat@users.noreply.github.com>
2020-03-19 10:39:50 -0500

    Update PagarTiquete.java

--- src/uiMain/menuconsola/PagarTiquete.java
+++ src/uiMain/menuconsola/PagarTiquete.java
@@ -24,26 +24,32 @@ public class PagarTiquete extends OpcionDeMenu{
 			System.out.println("Lista de sus tiquetes: ");
 			usuario.mostrarTiquetes();
 			System.out.println("Digite el numero de la lista, correspondiente al tiquete que desea pagar");
-			int numero = sc.nextInt();
 			
-			Tiquete tiquete = usuario.seleccionarTiquete(numero);
+			Tiquete tiquete=null;	
+			do {
+				int numero = sc.nextInt();
+				tiquete = usuario.seleccionarTiquete(numero);
+				if(tiquete.getRefPago().isPagado()) {
+					System.out.println("AVISO: El tiquete ya ha sido pagado");
+					break;
+				}else {
+					Pago pago = tiquete.getRefPago();
+					Tarjeta_des tarjeta = usuario.getAfiliacion();
+					
+					boolean transaccion = pago.transaccion(tarjeta);
+					if (transaccion == true) {
+						System.out.println("Transaccion exitosa");
+						System.out.println("Resumen de la transaccion: ");
+						System.out.println("Codigo del pago: " + pago.getCodigo());
+						System.out.println("Valor del tiquete: " + pago.getCostoTiquete());
+						System.out.println("Valor total (iva incluido): " + pago.getCostoTotal());
+					}
+					else {
+						System.out.println("No tienes saldo suficiente");
+					}
+				}
+			}while(tiquete==null);
 			
-			Pago pago = tiquete.getRefPago();
-			
-			Tarjeta_des tarjeta = usuario.getAfiliacion();
-			
-			boolean transaccion = pago.transaccion(tarjeta);
-			
-			if (transaccion == true) {
-				System.out.println("Transaccion exitosa");
-				System.out.println("Resumen de la transaccion: ");
-				System.out.println("Codigo del pago: " + pago.getCodigo());
-				System.out.println("Valor del tiquete: " + pago.getCostoTiquete());
-				System.out.println("Valor total (iva incluido): " + pago.getCostoTotal());
-			}
-			else {
-				System.out.println("No tienes saldo suficiente");
-			}
 		}else {
 			System.out.println("No posee tiquetes disponibles a pagar");
 		}
@@ -52,4 +58,3 @@ public class PagarTiquete extends OpcionDeMenu{
 	}
 	
 }
-
