40d6d3832b4218fdc5e92128c015842c86c50e0a
dmolinat <55594409+dmolinat@users.noreply.github.com>
2020-03-19 10:35:32 -0500

    Update CambiarAsiento.java

--- src/uiMain/menuconsola/CambiarAsiento.java
+++ src/uiMain/menuconsola/CambiarAsiento.java
@@ -23,47 +23,60 @@ public class CambiarAsiento extends OpcionDeMenu {
 		
 		System.out.println("Lista de sus tiquetes: ");
 		usuario.mostrarTiquetes();
+		
 		if (usuario.getTiquetes().isEmpty()) {
 			System.out.println("No tiene tiquetes asignados");
 			return;
-		}
-		
-		System.out.println("Digite el numero de la lista, correspondiente al tiquete al cual desea cambiar el asiento");
-		int numero = sc.nextInt();
-		
-		Tiquete tiquete = usuario.seleccionarTiquete(numero);
-		
-		System.out.println("Lista de asientos disponibles: ");
-		Funcion funcion = tiquete.getEvento();
-		boolean verAsientos = funcion.verAsientosDisponibles();
-		if (verAsientos == false) {
-			System.out.println("No hay asientos disponibles");
-			return;
-		}
-		
-		System.out.println("Por favor, escriba el codigo del asiento deseado");
-		System.out.println("El codigo debe tener la siguiente estructura: una letra al inicio (A-H) y un numero al final (0 al 9)");
-		System.out.println("Ejemplos: A0, B6, H9");
-		System.out.println("Asientos que empiezan por A y B son privados, tienen un costo de 10.000");
-		System.out.println("Los demas asientos son comunes, tienen un valor de 5.000");
-		
-		String codigoAsiento = sc.next();
-		Asiento asiento = funcion.buscarAsiento(codigoAsiento);
-		
-		if (asiento == null) {
-			System.out.println("Codigo de asiento invalido");
-			return;
-		}
-		else {
-			boolean cambiar = tiquete.cambiarAsiento(asiento);
-			if (cambiar == true) {
-				System.out.println("Asiento cambiado correctamente");
-			}
-			else {
-				System.out.println("El asiento no esta disponible, o el evento ya paso");
+		}else {
+			System.out.println("Digite el numero de la lista, correspondiente al tiquete al cual desea cambiar el asiento");
+			
+			Tiquete tiquete = null;
+			do {
+				int numero = sc.nextInt();
+				tiquete = usuario.seleccionarTiquete(numero);
+				if(tiquete==null) {
+					System.out.println("AVISO: Tiquete no disponible");
+				}
+			}while(tiquete==null);
+			
+			
+			
+			
+			System.out.println("Lista de asientos disponibles: ");
+			Funcion funcion = tiquete.getEvento();
+			boolean verAsientos = funcion.verAsientosDisponibles();
+			
+			if (verAsientos == false) {
+				System.out.println("No hay asientos disponibles");
 				return;
 			}
+			
+			System.out.println("Por favor, escriba el codigo del asiento deseado");
+			System.out.println("El codigo debe tener la siguiente estructura: una letra al inicio (A-H) y un numero al final (0 al 9)");
+			System.out.println("Ejemplos: A0, B6, H9");
+			System.out.println("Asientos que empiezan por A y B son privados, tienen un costo de 10.000");
+			System.out.println("Los demas asientos son comunes, tienen un valor de 5.000");
+			
+			String codigoAsiento = sc.next();
+			Asiento asiento = funcion.buscarAsiento(codigoAsiento);
+			
+			if (asiento == null) {
+				System.out.println("Codigo de asiento invalido");
+				return;
+			}
+			else {
+				boolean cambiar = tiquete.cambiarAsiento(asiento);
+				if (cambiar == true) {
+					System.out.println("Asiento cambiado correctamente");
+				}
+				else {
+					System.out.println("El asiento no esta disponible, o el evento ya paso");
+					return;
+				}
+			}
+
 		}
 		
+				
 	}
 }
