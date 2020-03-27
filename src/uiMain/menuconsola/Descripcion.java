2824e80ac98b12c1749bafb06c488c271f5c7f19
KeithKelsy <55594418+KeithKelsy@users.noreply.github.com>
2020-03-27 00:44:58 -0500

    Add files via upload

--- src/uiMain/menuconsola/Descripcion.java
+++ src/uiMain/menuconsola/Descripcion.java
@@ -7,10 +7,10 @@ public class Descripcion extends OpcionDeMenu {
 
 	@Override
 	public void ejecutar() {
-		Ini.getLD().setText("'La aplicaci√≥n ofrece una serie de herramientas\n"
-				+ "para el uso c√≥modo del usuario que ingrese. En √©l podr√° realizar\n"
+		Ini.getLD().setText("'La aplicaciÛn ofrece una serie de herramientas\n"
+				+ "para el uso cÛmodo del usuario que ingrese. En Èl podr· realizar\n"
 				+ "las operaciones necesarias para lograr obtener un tiquete a alguna\n"
-				+ "funci√≥n del usuario de forma satisfactoria. Desde escoger el evento\n"
+				+ "funciÛn del usuario de forma satisfactoria. Desde escoger el evento\n"
 				+ "con su asiento respectivo, hasta abstenerse de ir con nuestro control\n"
 				+ "de reembolsos.'");
 		
