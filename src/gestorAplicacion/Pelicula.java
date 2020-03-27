9b4fe975f620270082f3ee9a317dde9a875f0423
dmolinat <55594409+dmolinat@users.noreply.github.com>
2020-03-27 00:43:27 -0500

    Update Pelicula.java

--- src/gestorAplicacion/Pelicula.java
+++ src/gestorAplicacion/Pelicula.java
@@ -1,6 +1,7 @@
 package gestorAplicacion;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Scanner;
 
 
@@ -9,16 +10,87 @@ public class Pelicula {
 	private String nombre;
 	private String genero;
 	private String director;
-	private int añoLanzamiento;
+	private Integer añoLanzamiento;
     public static ArrayList<Pelicula> Cartelera = new ArrayList();
     private ArrayList<Funcion> funciones;
     
-    public Pelicula(String nombre,String genero,String director,int año){
+    public Pelicula(String nombre,String genero,String director,Integer año){
         this.nombre=nombre;
         this.genero=genero;
         this.director=director;
         this.añoLanzamiento=año;
     }
+    
+    public static Pelicula getPeliWithName(String name) {
+    	for(int i=0;i<Cartelera.size();i++) {
+    		Pelicula P = Cartelera.get(i);
+    		String Np = P.getNombre();
+    		if(Np.equals(name)) {
+    			return P;
+    		}
+    	}
+    	return null;
+    }
+    
+    
+    public static int getSalaWithNameAndDate(String name,String Date) {
+    	Pelicula P = null;
+    	for(int i=0;i<Cartelera.size();i++) {
+    		P = Cartelera.get(i);
+    		String Np = P.getNombre();
+    		if(Np.equals(name)) {
+    			break;
+    		}
+    	}
+    	ArrayList<Funcion> aux = P.getFuncions();
+    	for(int j=0; j<P.getFuncions().size();j++) {
+    		
+    		String fecha1 = Date.substring(7);	
+    		String f2 = ""+aux.get(j).getFecha1();
+    		
+    		
+    		if(f2.trim().equals(fecha1.trim())) {
+    			return aux.get(j).getSala();
+    		}
+    	}
+    	return -1;
+    }
+    
+    public static ArrayList<Asiento> getAsientosWithNameAndDate(String name,String Date){
+    	Pelicula P = null;
+    	for(int i=0;i<Cartelera.size();i++) {
+    		P = Cartelera.get(i);
+    		String Np = P.getNombre();
+    		if(Np.equals(name)) {
+    			break;
+    		}
+    	}
+    	
+    	ArrayList<Funcion> aux = P.getFuncions();
+    	for(int j=0; j<P.getFuncions().size();j++) {
+    		
+    		String fecha1 = Date.substring(7);	
+    		String f2 = ""+aux.get(j).getFecha1();
+    		
+    		
+    		if(f2.trim().equals(fecha1.trim())) {
+    			return aux.get(j).getAsientos();
+    		}
+    	}
+    	return new ArrayList<Asiento>();
+    }
+    
+    public String getGenero() {
+    	return this.genero;
+    }
+    
+    public String getDirector() {
+    	return this.director;
+    }
+    public Integer getAñoLanzamiento() {
+    	return this.añoLanzamiento;
+    }
+    
     public String verInfo(){
         return "Nombre: "+nombre + " Genero: "+ genero + " Director: "+director + " Año de lanzamiento: "+añoLanzamiento;
     }
@@ -33,7 +105,6 @@ public class Pelicula {
     }
     
     public static Pelicula ElegirPelicula(int i){
-        //verifica si existe esta pelicula en cartelera
         if (i>=0 && i< Cartelera.size()){
             return Cartelera.get(i);
         } 
@@ -57,7 +128,6 @@ public class Pelicula {
     }
     
     public Funcion elegirFuncion(int i){
-        //verifica si existe ese numero de funcion
         if (i>=0 && i< funciones.size()){
             return funciones.get(i);
         }
